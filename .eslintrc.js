97fe04ce03063e4d70b9b6ea2d5323506bd109b8
Tiziano Puppi <tiziano.puppi@gmail.com>
2019-04-05 15:09:02 +0200

    Better eslint rc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  root: true,
-  extends: 'standard',
-  env: {
-    browser: true,
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
   },
+  'extends': 'standard',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+  }
 }
